6bcc6e350a297cec2ea17409a1a9670fcdb447ef
victor-hugo-de-alencar-morais <vham17.com.br@gmail.com>
2026-04-09 22:45:33 -0300

    implementação da loginScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,93 @@
-import {View,Text} from "react-native";
+import {View,Text,TextInput, TouchableOpacity, StyleSheet } from "react-native";
+
 
 export default function LoginScreen(){
     return(
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <View style={styles.center}>
+            <Text style={styles.title}>Control</Text>
+            <TextInput
+            placeholder="E-mail"
+            style={styles.input} />
+
+            <TextInput
+            placeholder="Senha"
+            secureTextEntry
+            style={styles.input} />
+
+            <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress ={() => navigation.navigate("Cadastro")}>
+                <Text style={styles.link}>
+                    Não tem conta ? Criar conta
+                </Text>
+            </TouchableOpacity>
         </View>
-    )
+
+        <TouchableOpacity
+        style={styles.aboutButton}
+        onPress={() => navigation.navigate(Sobre)}>
+        <Text style={styles.aboutText}>
+            Sobre o aplicativo
+        </Text>
+        </TouchableOpacity>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "#fff",
+        padding:20
+    },
+
+    center:{
+        flex:1,
+        justifyContent:"center"
+    },
+
+    title:{
+        fontSize:32,
+        fontWeight:"bold",
+        marginBottom:40,
+        textAlign:"center"
+    },
+
+    input:{
+        borderWidth:1,
+        borderColor:"#ccc",
+        borderRadius:8,
+        padding:12,
+        marginBottom:15
+    },
+
+    button:{
+        backgroundColor:"#1E88E5",
+        padding:15,
+        borderRadius:8,
+        alignItems:"center"
+    },
+
+    buttonText:{
+        color:"#fff",
+        fontWeight:"bold"
+    },
+    link:{
+        marginTop:15,
+        textAlign:"center",
+        color:"#1E88E5"
+    },
+
+    aboutButton:{
+        alignItems: "center",
+        marginBottom:20
+    },
+
+    aboutText:{
+        color:"#999",
+        fontSize:14
+    }
+})
