6ff1a9be7642f3ea59fa3a4ce85a30fea962d2bf
Victor_M0rais <93052406+victor-hugo-de-alencar-morais@users.noreply.github.com>
2026-06-04 00:35:54 +0000

    feito a entidadeScreen

--- src/screens/EntidadeScreen.tsx
+++ src/screens/EntidadeScreen.tsx
@@ -1,9 +1,118 @@
-import {View,Text} from "react-native";
+import {View,Text, TextInput, TouchableOpacity,StyleSheet,ScrollView} from "react-native";
+
+import {SafeAreaView} from "react-native-safe-area-context"
 
 export default function EntidadeScreen(){
     return(
-        <View>
-            <Text>Entidade</Text>
-        </View>
-    )
+        <SafeAreaView style={styles.safeArea}>
+
+            <ScrollView
+            contentContainerStyle={styles.container}
+            showVerticalScrollIndicator={false}
+            />
+
+        <Text style={styles.title}>Cadastrar Entidade</Text>
+
+        <TextInput 
+         placeholder="Nome"
+         style={styles.input}
+         />
+
+         <TextInput 
+         placeholder="CEP"
+         keyboardType="numeric"
+         style={styles.input}
+         />
+
+         <TextInput 
+         placeholder="Endereço"
+         style={styles.input}
+         />
+
+         <TextInput 
+         placeholder="Número"
+         keyboardType="numeric"
+         style={styles.input}
+         />
+
+         <TextInput 
+         placeholder="Bairro"
+         style={styles.input}
+         />
+
+         <TextInput 
+         placeholder="Cidade"
+         style={styles.input}
+         />
+
+        <TextInput 
+         placeholder="Estado"
+         style={styles.input}
+         />
+
+         <TextInput 
+         placeholder="Telefone"
+         keyboardType="phone-pad"
+         style={styles.input}
+         />
+
+        <TextInput 
+         placeholder="Observação"
+         multiline
+         style={{styles.input, styles.textArea}}
+         />
+
+         <TouchableOpacity style={styles.button}>
+            <Text style={Styles.buttonText}>Salvar</Text>
+         </TouchableOpacity>
+
+        </ScrollView>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex:1,
+        backgroundColor: "#fff"
+    },
+
+    container: {
+        padding:20,
+        paddingBottom: 80
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20,
+        textAlign:"center"
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: "#CCC",
+        borderRadius: 8,
+        padding: 12,
+        marginBottom: 15
+    },
+
+    textArea: {
+        height: 100,
+        textAlignVertical: "top"
+    },
+
+    button: {
+        backgroundColor: "#1E88E5",
+        padding:15,
+        borderRadius: 8,
+        alignItems: "center",
+        marginTop: 10
+    },
+
+    buttonText: {
+        color: "#fff",
+        fontWeight: "bold"
+    }
+
+});
